f7ff088f3be7f1855f3847097b136e66eaf04ffa
Rizwan Zaheer <rizwanzaheer003@gmail.com>
2018-08-26 23:31:15 +0500

    adding api key

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome Rizwan, In Vue.js App "/> -->
+    <img alt="Vue logo" src="./assets/logo.png">
     <SearchBar @termChange="onTermChange"></SearchBar>
+    <HelloWorld msg="Welcome Rizwan, In Vue.js App "/>
   </div>
 </template>
 
 <script>
-// import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from "./components/HelloWorld.vue";
 import SearchBar from "./components/SearchBar.vue";
+const API_KEY = 'AIzaSyBDHxTtzOZM4d_0inRjfvyQc8YZh9T4O-k';
 
 export default {
   name: "app",
   components: {
-    // HelloWorld,
+    HelloWorld,
     SearchBar
   },
   methods: {
@@ -25,12 +26,12 @@ export default {
 </script>
 
 <style>
-/* #app {
+#app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-} */
+}
 </style>
