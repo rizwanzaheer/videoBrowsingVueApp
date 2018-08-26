829cd0cc4abf2d850bb7744958a7f8fd1905526d
Rizwan Zaheer <rizwanzaheer003@gmail.com>
2018-08-26 23:59:14 +0500

    binding video list to the parent app & showing on a page

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <SearchBar @termChange="onTermChange"></SearchBar>
+    <VideoList />
     <HelloWorld msg="Welcome Rizwan, In Vue.js App "/>
   </div>
 </template>
@@ -10,27 +11,32 @@
 import axios from "axios";
 import HelloWorld from "./components/HelloWorld.vue";
 import SearchBar from "./components/SearchBar.vue";
+import VideoList from "./components/VideoList.vue";
+
 const API_KEY = "AIzaSyBDHxTtzOZM4d_0inRjfvyQc8YZh9T4O-k";
 
 export default {
   name: "app",
   components: {
     HelloWorld,
-    SearchBar
+    SearchBar,
+    VideoList
   },
   methods: {
     onTermChange: searchTerm => {
       console.log("searchTerm is: ", searchTerm);
-      axios.get("https://www.googleapis.com/youtube/v3/search", {
-        params: {
-          key: API_KEY,
-          type: "video",
-          part: "snippet",
-          q: searchTerm
-        }
-      }).then(data => {
-        console.log('youtube response is: ', data);
-      });
+      axios
+        .get("https://www.googleapis.com/youtube/v3/search", {
+          params: {
+            key: API_KEY,
+            type: "video",
+            part: "snippet",
+            q: searchTerm
+          }
+        })
+        .then(data => {
+          console.log("youtube response is: ", data);
+        });
     }
   }
 };
--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,8 +1,12 @@
 <template>
-  
+  <ul>
+    <li>
+      Video List
+    </li>
+  </ul>
 </template>
 <script>
 export default {
-  
+  name: 'VideoList'
 }
 </script>
