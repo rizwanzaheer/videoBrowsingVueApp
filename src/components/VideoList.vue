cda5a946c1f443113d859ca8d710adf4ff424f96
Rizwan Zaheer <rizwanzaheer003@gmail.com>
2018-08-28 19:32:06 +0500

    adding props in videolist

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -3,10 +3,17 @@
     <li>
       Video List
     </li>
+    <li>
+      props length:{{ videos.length }}
+    </li>
   </ul>
 </template>
 <script>
 export default {
-  name: 'VideoList'
-}
+  name: "VideoList",
+  props: ["videos"]
+  // props: {
+  //   videos: Array
+  // },
+};
 </script>
