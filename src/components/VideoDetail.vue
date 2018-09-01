e8885f0937d1658790fa94154199b765c2ea9c81
Rizwan Zaheer <rizwanzaheer003@gmail.com>
2018-09-02 02:04:41 +0500

    adding style on video detail component

--- src/components/VideoDetail.vue
+++ src/components/VideoDetail.vue
@@ -1,6 +1,14 @@
 <template>
   <div v-if="video">
-    {{ video.snippet.title }}
+    <div class="details ">
+    <h4>
+      {{ video.snippet.title }}
+    </h4>
+    <p>
+      {{ video.snippet.description }}
+    </p>
+
+    </div>
   </div>
 </template>
 
@@ -10,3 +18,14 @@ export default {
   props: ["video"]
 };
 </script>
+
+
+<style scoped>
+.details {
+  margin-top: 10px;
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+}
+</style>
+
