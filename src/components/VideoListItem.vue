7db71bfd8b11f141dd42658806d6e6866d00c26a
Rizwan Zaheer <rizwanzaheer003@gmail.com>
2018-09-02 00:36:10 +0500

    displaying post thumbnails

--- src/components/VideoListItem.vue
+++ src/components/VideoListItem.vue
@@ -1,11 +1,19 @@
 <template>
-  <li class="list-group-item">
+  <li class="list-group-item media">
+    <img :src="thumbnailUrl" alt="" class="mr-3">
+   <div class="media-body">
     {{ video.snippet.title}}
+   </div>
   </li>
 </template>
 <script>
 export default {
-  name: 'VideoListItem',
-  props:['video']
-}
+  name: "VideoListItem",
+  props: ["video"],
+  computed: {
+    thumbnailUrl() {
+      return this.video.snippet.thumbnails.default.url;
+    }
+  }
+};
 </script>
